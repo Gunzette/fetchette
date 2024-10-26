b0b38a72729472581adcdbf65a024bc705cbfdc9
Gunzette <gunzettedev@gmail.com>
2024-10-26 16:40:21 +0200

    first test of putting modules next to logo

--- fetchette.go
+++ fetchette.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gunzette/fetchette/modules"
 	"github.com/gunzette/fetchette/termcolor"
 )
 
 func main() {
-	fmt.Println(termcolor.GetColoredLogoString("lesbianColors.json"))
+	logo := termcolor.GetColoredLogoString("archColors.json")
+
+	for i, line := range strings.Split(logo, "\n") {
+		if line == "" {
+			continue
+		}
+
+		fmt.Println(line, "a", i)
+	}
+
 	fmt.Println(modules.GetOS())
 	fmt.Println(modules.GetUserAtHost())
 	fmt.Println(modules.GetKernel())
